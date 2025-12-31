ebc8c5d994451dc6b7a2ecbd4cba431dc43f7d16
GAURAB <gaurabkhadka4082@gmail.com>
2025-12-31 11:09:40 +0545

    classwork

--- lib/features/batch/presentation/view_model/batch_viewmodel.dart
+++ lib/features/batch/presentation/view_model/batch_viewmodel.dart
@@ -1,15 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-// import 'package:lost_n_found/features/batch/domain/usecases/create_batch_usecase.dart';
-// import 'package:lost_n_found/features/batch/domain/usecases/delete_batch_usecase.dart';
+import 'package:lost_n_found/features/batch/domain/usecases/create_batch_usecase.dart';
+import 'package:lost_n_found/features/batch/domain/usecases/delete_batch_usecase.dart';
 import 'package:lost_n_found/features/batch/domain/usecases/get_all_batch_usecase.dart';
-// import 'package:lost_n_found/features/batch/domain/usecases/update_batch_usecase.dart';
+import 'package:lost_n_found/features/batch/domain/usecases/update_batch_usecase.dart';
 import 'package:lost_n_found/features/batch/presentation/states/batch_state.dart';
 
 class BatchViewmodel extends Notifier<BatchState> {
   late final GetAllBatchUsecase _getAllBatchUsecase;
-  // late final CreateBatchUsecase _createBatchUsecase;
-  // late final UpdateBatchUsecase _updateBatchUsecase;
-  // late final DeleteBatchUsecase _deleteBatchUsecase;
+  late final CreateBatchUsecase _createBatchUsecase;
+  late final UpdateBatchUsecase _updateBatchUsecase;
+  late final DeleteBatchUsecase _deleteBatchUsecase;
 
   @override
   BatchState build() {
@@ -28,6 +28,54 @@ class BatchViewmodel extends Notifier<BatchState> {
         errorMessage: failure.message,
       ),
       (batches) => state.copyWith(status: BatchStatus.loaded, batches: batches),
+    );
+  }
+
+  Future<void> createBatch(String batchName) async {
+    state = state.copyWith(status: BatchStatus.loading);
+
+    final result = await _createBatchUsecase(
+      CreateBatchUsecaseParams(batchName: batchName),
+    );
+
+    result.fold(
+      (failure) => state = state.copyWith(
+        status: BatchStatus.error,
+        errorMessage: failure.message,
+      ),
+      (success) => state = state.copyWith(status: BatchStatus.loaded),
+    );
+  }
+
+  Future<void> updateBatch(String batchId, String batchName) async {
+    state = state.copyWith(status: BatchStatus.loading);
+
+    final result = await _updateBatchUsecase(
+      UpdateBatchUsecaseParams(batchId: batchId, batchName: batchName),
+    );
+
+    result.fold(
+      (failure) => state = state.copyWith(
+        status: BatchStatus.error,
+        errorMessage: failure.message,
+      ),
+      (success) => state = state.copyWith(status: BatchStatus.updated),
+    );
+  }
+
+  Future<void> deleteBatch(String batchId) async {
+    state = state.copyWith(status: BatchStatus.loading);
+
+    final result = await _deleteBatchUsecase(
+      DeleteBatchUsecaseParams(batchId: batchId),
+    );
+
+    result.fold(
+      (failure) => state = state.copyWith(
+        status: BatchStatus.error,
+        errorMessage: failure.message,
+      ),
+      (success) => state = state.copyWith(status: BatchStatus.deleted),
     );
   }
 }
--- lib/features/category/domain/usecases/get_all_category_usecase.dart
+++ lib/features/category/domain/usecases/get_all_category_usecase.dart
@@ -1,16 +1,17 @@
 import 'package:dartz/dartz.dart';
 import 'package:lost_n_found/core/error/failures.dart';
 import 'package:lost_n_found/core/usecases/app_usecase.dart';
+import 'package:lost_n_found/features/category/domain/entities/category_entity.dart';
 import 'package:lost_n_found/features/category/domain/repository/category_repository.dart';
 
 class GetAllCategoryUsecase implements UseCaseWithoutParams {
-  ICategoryRepository _categoryRepository;
+  final ICategoryRepository _categoryRepository;
 
   GetAllCategoryUsecase({required ICategoryRepository categoryRepository})
     : _categoryRepository = categoryRepository;
 
   @override
-  Future<Either<Failure, dynamic>> call() {
+  Future<Either<Failure, List<CategoryEntity>>> call() {
     return _categoryRepository.getAllCategories();
   }
 }
